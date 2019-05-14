85ea82224b7533b9dd1dd1864747429df34407df
Robert <robertgama.cs@gmail.com>
2019-05-14 13:02:52 -0700

    refactored dates to work with if modified since headers

--- articles.py
+++ articles.py
@@ -29,18 +29,6 @@ def not_found(error=None):
 
     return resp
 
-# @app.errorhandler(304)
-# def not_modified(error=None):
-#     message = {
-#         'status': 304,
-#         'message': 'Not Modified: ' + request.url
-#     }
-
-#     resp = jsonify(message)
-#     resp.status_code = 304
-
-#     return resp
-
 @app.route('/articles')
 def root():
     return "testing testing..."
@@ -48,25 +36,17 @@ def root():
 # curl -i --header 'Content-Type: application/json' http://localhost/articles/66394710-9d34-4418-875f-804083cf849d/
 @app.route('/articles/<id>/', methods=['GET'])
 def article(id):
-
-    #query = session.prepare("SELECT * FROM testkeyspace.articles WHERE article_id = ?")
-    #articles = session.execute(query, str(id))
-     # is_conditional = request.headers.get('If-Modified-Since') 
-    # or request.headers.get('If-None-Match')
     
-    # if 'If-Modified-Since' in request.headers:
-    #     if request.if_modified_since > datemod:
-    #         print("Testing", file=sys.stderr)
-
-
-    # print(str(request.headers.get("If-Modifed-Since")), file=sys.stderr)
-
-    # print(str(datemod), file=sys.stderr)
-
-    articles = session.execute("SELECT * FROM testkeyspace.articles WHERE article_id ="+str(id))
-    datemod = datetime.strptime(str(articles[0].last_modified), "%Y-%m-%d %H:%M:%S.%f")
-    lastmodified = datemod.strftime("%a, %d %b %Y %I:%M:%S GMT")
+    articles = session.execute("SELECT * FROM testkeyspace.articles WHERE article_id =" + str(id))
+  
+    # Datatype: String
+    lastmod_str = datetime.strftime(articles[0].last_modified,"%a, %d %b %Y %I:%M:%S GMT") 
     
+    # Datatype: Date
+    lastmod_date = datetime.strptime(str(lastmod_str), "%a, %d %b %Y %I:%M:%S GMT")
+    
+    # Example: Mon, 05 Oct 2015 07:28:00 GMT
+
     if request.method == 'GET':
         if articles is not None:
             message = {
@@ -74,10 +54,10 @@ def article(id):
                     }
             resp = jsonify(message)
             resp.status_code = 200
-            resp.headers['Last-Modified'] = str(lastmodified)
+            resp.headers['Last-Modified'] = lastmod_str
 
-            if 'If-Modified-Since' in request.headers:
-                if request.if_modified_since < lastmodified:
+            if request.headers.get("If-Modified-Since") is not None:
+                if request.if_modified_since < lastmod_date:
                     return resp
                 else:
                     resp.status_code = 304
@@ -100,7 +80,7 @@ def new_article():
         content = request.json['content']
 
         stmt = session.prepare("INSERT INTO articles (article_id, author, content, date_published, last_modified, title) VALUES (uuid(), ?, ?, toTimeStamp(now()), toTimeStamp(now()), ?)")
-        query = session.execute(stmt, (auth.username, content, title))
+        session.execute(stmt, (auth.username, content, title))
 
         message = { 'message': 'Article created!'}
         resp = jsonify(message)
@@ -155,34 +135,43 @@ def delete_article(id):
 @app.route('/articles/retrieve_articles/<int:num>/', methods=['GET'])
 def retrieve_articles(num):
     
-    # articles = dbf.query_db("SELECT * FROM articles ORDER BY article_id DESC LIMIT (?)", [num])
-
     articles = session.execute("SELECT * FROM testkeyspace.articles LIMIT "+ str(num))
 
-    last_modified = articles[0].last_modified
-    # will hold all articles 
+    # Store last modified as string from  lastest article in articles 
+    lastmod_str = datetime.strftime(articles[0].last_modified,"%a, %d %b %Y %I:%M:%S GMT")
+    
+    # Store last modified as date type from  lastest article in articles 
+    lastmod_date = datetime.strptime(str(lastmod_str), "%a, %d %b %Y %I:%M:%S GMT")
+
     articles_msg = []
 
-    if request.method == 'GET' and articles is not None:
-        for article in articles:
-            articles_msg.append(
-                {
-                    "article_id": article.article_id,
-                    "author": article.author,
-                    "title": article.title,
-                    "content": article.content,
-                    "date_published": article.date_published,
-                    "last_modified": article.last_modified
-                }   
-            )
-        
-        resp = jsonify(articles_msg)
-        resp.status_code = 200
-        resp.headers['Last-Modified'] = str(datetime.strptime(str(last_modified), "%Y-%m-%d %H:%M:%S.%f"))
-        return resp
-
-    elif articles is None:
-        return not_found()
+    if request.method == 'GET':
+        if articles is not None:
+            for article in articles:
+                articles_msg.append(
+                    {
+                        "article_id": article.article_id,
+                        "author": article.author,
+                        "title": article.title,
+                        "content": article.content,
+                        "date_published": article.date_published,
+                        "last_modified": article.last_modified
+                    }   
+                )
+            
+            resp = jsonify(articles_msg)
+            resp.status_code = 200
+            resp.headers['Last-Modified'] = 
+            
+            if request.headers.get("If-Modified-Since") is not None:
+                if request.if_modified_since < lastmod_date:
+                    return resp
+                else:
+                    resp.status_code = 304
+                    return resp            
+            return resp
+        else:
+            return not_found()
 
 # #  curl -i --header 'Content-Type: application/json' http://localhost/articles/metadata/2/
 @app.route('/articles/metadata/<int:num>/', methods=['GET'])
@@ -190,28 +179,41 @@ def retrieve_metadata(num):
 
     articles = session.execute("SELECT * FROM testkeyspace.articles LIMIT "+ str(num))
 
+    # Store last modified as string from  lastest article in articles 
+    lastmod_str = datetime.strftime(articles[0].last_modified,"%a, %d %b %Y %I:%M:%S GMT")
     
-    #articles = dbf.query_db("SELECT * FROM articles ORDER BY article_id DESC LIMIT (?)", [num])
-    # will hold all articles 
+    # Store last modified as date type from  lastest article in articles 
+    lastmod_date = datetime.strptime(str(lastmod_str), "%a, %d %b %Y %I:%M:%S GMT")
+
+
     articles_msg = []
 
-    if request.method == 'GET' and articles is not None:
-        for article in articles:
-            articles_msg.append(
-                {
-                    "author": article.author,
-                    "title": article.title,
-                    "content": article.content,
-                    "date_published": article.date_published,
-                    "url": "http:localhost/articles/retrieve_metadata/" + str(article.article_id)
-                }   
-            )
-        
-        resp = jsonify(articles_msg)
-        resp.status_code = 200
-        # Not sure if this is correct, we can only add one response header
-        # resp.headers['Last-Modified'] = str(datetime.strptime(articles[0]['last_modified'], "%Y-%m-%d %H:%M:%f"))
-        return resp
-    elif articles is None:
-        return not_found()
+    if request.method == 'GET':
+        if articles is not None:
+            if request.headers.get("If-Modified-Since") is not None:
 
+                for article in articles:
+                    articles_msg.append(
+                        {
+                            "author": article.author,
+                            "title": article.title,
+                            "content": article.content,
+                            "date_published": article.date_published,
+                            "url": "http:localhost/articles/retrieve_metadata/" + str(article.article_id)
+                        }   
+                    )
+                
+                resp = jsonify(articles_msg)
+                resp.status_code = 200
+                resp.headers['Last-Modified'] = lastmod_str
+
+                if request.last_modified < lastmod_date:
+                    return resp
+                else:
+                    resp.status_code = 304
+                    return resp
+            else:
+                return resp
+        else:
+            return not_found()
+            
